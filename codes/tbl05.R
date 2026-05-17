256a2a03da97a0f7645354451756ddd4ba1e1cf8
vijay pratap <pratap.082020@gmail.com>
2026-05-17 17:58:07 +0530

    updated

--- codes/tbl05.R
+++ codes/tbl05.R
@@ -1,5 +1,5 @@
 #---------------------------------------------------------
-# Program Name : tbl04
+# Program Name : tbl05
 # Study        : 
 # Purpose      : Descriptive statistics for Age variable
 # Author       : Vijay Pratap
@@ -18,18 +18,19 @@ for(pkg in packages) {
 }
 
 adsl<-tribble(
-  ~studyid,~usubjid,~trt01pn,~age,~height,~asex,~arace,~fasfl,~trt01p,
+  ~studyid,~usubjid,~trt01pn,~age,~heightbl,~asex,~arace,~fasfl,~trt01p,
   "MYCSG","MYCSG-1001",1,23,165,"Female","Asian","Y","Dose level 1",
   "MYCSG","MYCSG-1002",3,68,NA,"","Black or African American","Y","Dose level 3",
   "MYCSG","MYCSG-1003",3,NA,153,"Male","","Y","Dose level 3",
   "MYCSG","MYCSG-1004",3,35,152,"Female","White","Y","Dose level 3",
   "MYCSG","MYCSG-1006",3,54,172,"Male","Not reported","Y","Dose level 3",
-  "MYCSG","MYCSG-1007",3,54,180,"Male","White","N","Dose level 3",
+  "MYCSG","MYCSG-1007",3,45,165,"Male","Not reported","N","Dose level 3",
 )
 
 
-tbl <- adsl %>%
-  filter(fasfl == "Y") %>%   # Full Analysis Set
+
+tbl01 <- adsl %>%
+  filter(fasfl == "Y") %>%   
   select(trt01p, asex, arace) %>% mutate(
     asex = if_else(asex=="", "Missing", asex),
     arace = if_else(arace=="", "Missing", arace)
@@ -41,28 +42,65 @@ tbl <- adsl %>%
     statistic = list(
       all_categorical() ~ "{n} ({p}%)"   # show both frequency and percent
     )
-  ) %>% add_overall(last = T, col_label="**Total<br>(N = {n})**") %>%
+  ) %>% add_overall(last = T) %>%
   modify_table_body(
     ~.x %>% mutate(
       label = if_else(label == "asex", "Sex, n (%)", label),
       label = if_else(label == "arace", "Race, n (%)", label)
     )
-  ) %>% modify_footnote(all_stat_cols() ~ NA) %>%
-  modify_header(
-    label ~ "**Statistic**",
-    stat_1 ~ "**Dose level 1<br>(N = {n})**",
-    stat_2 ~ "**Dose level 3<br>(N = {n})**"
+  ) %>% modify_footnote(all_stat_cols() ~ NA)
+
+  
+  
+tbl01
+
+
+
+tbl02 <- adsl %>% filter(fasfl == "Y") %>%
+  select(trt01p, age, heightbl) %>%
+  tbl_summary(
+    by = trt01p,
+    type = c(age, heightbl) ~ "continuous2",
+    statistic = c(age, heightbl) ~ c(
+      "{N_nonmiss} ({N_miss})",
+      "{mean} ({sd})",
+      "{median}",
+      "{p25}, {p75}",
+      "{min}, {max}"
+    ),
+    digits = c(age, heightbl) ~ c(0, 1, 1, 1, 0),
+    missing = "no"
   ) %>%
-  modify_caption(
-    "**Summary for categorical variables<br>Full Analysis Set**"
+  add_overall() %>%
+  modify_table_body(
+    ~.x %>% mutate(
+      label = if_else(label=="age", "Age (years)", label),
+      label = if_else(label=="heightbl", "Height (cm)", label),
+      label = if_else(label=="N Non-missing (N Missing)", "n (missing)", label)
+    )
   )
-  
-  
-tbl
-gt_tbl <- as_gt(tbl)
+
+tbl02
+
+
+
+
+tbl01
+tbl02
+
+complete_tbl <- tbl_stack(list(tbl02, tbl01)) %>% 
+  as_gt() %>% gt::cols_move_to_end(columns = "stat_0") %>%  
+  gt::tab_header(
+    title = md("Demographic and Baseline Characteristics"),
+    subtitle = md("Full Analysis Set")
+  )
+
+
+
+complete_tbl
 gtsave(
-  gt_tbl,
-  "output/tbl04.pdf"
+  complete_tbl,
+  "output/tbl05.pdf"
 )
 
 
